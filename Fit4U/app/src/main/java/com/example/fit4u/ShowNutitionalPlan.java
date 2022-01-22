47dc8ad0c6fbafca598867a44a9fdb5239f675b5
António Barbosa <1190404@isep.ipp.pt>
2022-01-22 04:18:00 +0100

    show nutritional plan layout

---
+++ Fit4U/app/src/main/java/com/example/fit4u/ShowNutitionalPlan.java
@@ -0,0 +1,14 @@
+package com.example.fit4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowNutitionalPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_nutitional_plan);
+    }
+}
