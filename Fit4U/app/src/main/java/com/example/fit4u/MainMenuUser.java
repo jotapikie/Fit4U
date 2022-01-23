caeaf4ad08ff75b1756414d160e830e85570276b
António Barbosa <1190404@isep.ipp.pt>
2022-01-23 05:00:01 +0100

    layout user e pt

---
+++ Fit4U/app/src/main/java/com/example/fit4u/MainMenuUser.java
@@ -0,0 +1,14 @@
+package com.example.fit4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenuUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu_user);
+    }
+}
