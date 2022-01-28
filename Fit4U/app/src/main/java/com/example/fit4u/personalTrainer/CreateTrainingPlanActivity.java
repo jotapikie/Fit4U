61ee06bebc787699bb0ae575a10f465f98143d4c
António Barbosa <1190404@isep.ipp.pt>
2022-01-28 13:34:40 +0100

    commit

--- Fit4U/app/src/main/java/com/example/fit4u/personalTrainer/CreateTrainingPlanActivity.java
+++ Fit4U/app/src/main/java/com/example/fit4u/personalTrainer/CreateTrainingPlanActivity.java
@@ -1,4 +1,59 @@
 package com.example.fit4u.personalTrainer;
 
-public class CreateTrainingPlanActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fit4u.*;
+
+public class CreateTrainingPlanActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner;
+    private static final String[] paths = {"Exercise 1", "Exercise 2", "Exercise 3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_training_plan);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreateTrainingPlanActivity.this,
+                R.layout.spinner_design, paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setPrompt("Select one of the exercises");
+        spinner.setAdapter(
+                new NothingSelectedSpinnerAdapter(
+                        adapter,
+                        R.layout.contact_spinner_row_nothing_selected,
+                        // R.layout.contact_spinner_nothing_selected_dropdown, // Optional
+                        this));
+        spinner.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+
 }
